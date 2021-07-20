16d7ec6d146bd5673ba0b6e3f064858b333e13ad
Mike R <cr8syoki@gmail.com>
2021-07-21 08:03:47 +0900

    remove .DS_Store files

--- python-101-main/projects/mover.py
+++ python-101-main/projects/mover.py
@@ -2,13 +2,23 @@
 # from one folder to another
 
 # Import pathlib
-
+from os import path
+import pathlib
+import shutil
 # Find the path to my Desktop
-
+desktop = pathlib.Path.home() / 'Desktop'
 # Create a new folder
+new_folder = pathlib.Path('/Users/m_a_t/Desktop/codingnomads')
 
 # Filter for screenshots only
-
+for x in desktop.iterdir():
+    if x.is_file() and x.suffix == '.png':
+        try:
+            if not new_folder.exists():
+                new_folder.mkdir()
+                shutil.move(x, new_folder)
+        except FileExistsError as err:
+            print('FileExists')
 # Create a new path for each file
 
 # Move the screenshot there
--- python-201-main/03_file-input-output/03_04_pathlib_reader.py
+++ python-201-main/03_file-input-output/03_04_pathlib_reader.py
@@ -1,3 +1,14 @@
 # Refactor your file counter script to use `pathlib` also for
 # reading and writing to your CSV file. Make sure to handle the
 # path in a way so that you can run the script from anywhere.
+
+import csv
+import pathlib
+
+path_to_csv = pathlib.Path("/Users/m_a_t/Documents/CodingNomads/labs/python-201-main/03_file-input-output/file-counter/filecounts.csv")
+
+with open(path_to_csv, "r") as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames=["Folder", "CSV", "MD", "PNG"])
+    counts = list(reader)
+
+print(counts)
