5c1b396bfbfa24ec82514f8a9ef27b0953127c86
Mike R <cr8syoki@gmail.com>
2021-07-27 20:28:50 +0900

    lab for venv lab

--- python-201-main/05_venvs-and-packages/05_04_environment_settings.py
+++ python-201-main/05_venvs-and-packages/05_04_environment_settings.py
@@ -6,3 +6,11 @@
 # 
 # Then write the necessary code to access and print the values of these
 # two environment variables in this script.
+
+import os 
+
+secret1 = os.environ['ENVIRONMENT']
+secret2 = os.environ['SECRET']
+
+print(secret1)
+print(secret2)
