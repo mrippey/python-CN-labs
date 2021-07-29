85914b6c8e649b040fe252879393eae98f03500b
Mike R <cr8syoki@gmail.com>
2021-07-29 20:11:59 +0900

    finished final labs for module

--- python-201-main/06_advanced-python-concepts/06_13_void_return.py
+++ python-201-main/06_advanced-python-concepts/06_13_void_return.py
@@ -1,2 +1,11 @@
 # Write a lambda function that does not take in an arguments 
 # but returns a value. Print the return value.
+
+
+c = lambda: print(1)
+
+print(c())
+
+greet = lambda : print('Hello World!')
+
+print(greet())
