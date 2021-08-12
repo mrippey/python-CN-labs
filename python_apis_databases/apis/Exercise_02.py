88de86f3de78492ef66bad6c3abc0cc180b2a496
Mike <30969281+mrippey@users.noreply.github.com>
2021-08-12 19:37:25 +0900

    Update Exercise_02.py

--- python_apis_databases/apis/Exercise_02.py
+++ python_apis_databases/apis/Exercise_02.py
@@ -3,3 +3,15 @@ Building on the previous example, create a list of all of the emails of the user
 the list to the console.
 
 '''
+
+import requests 
+
+
+url = 'http://demo.codingnomads.co:8080/tasks_api/users'
+
+request_response = requests.get(url)
+
+json_response = request_response.json()
+
+for data in json_response['data']:
+    print(data['email'])
