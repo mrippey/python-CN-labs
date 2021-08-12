3be29ead7d1257a85e9f34cd7929725f7e5323a4
Mike <30969281+mrippey@users.noreply.github.com>
2021-08-12 19:36:58 +0900

    Update Exercise_01.py

--- python_apis_databases/apis/Exercise_01.py
+++ python_apis_databases/apis/Exercise_01.py
@@ -8,7 +8,15 @@ Print out:
     - the status code
     - the encoding of the response
     - the text of the response body
-
-
-
 '''
+
+
+import requests 
+
+url = 'http://demo.codingnomads.co:8080/tasks_api/users'
+
+request_response = requests.get(url)
+
+print(request_response.status_code)
+print(request_response.content)
+print(request_response.text)
