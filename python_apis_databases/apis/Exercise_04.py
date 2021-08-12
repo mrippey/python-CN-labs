9c9a06cfa7d7e71632b05f4d9e9edc87473eada5
Mike <30969281+mrippey@users.noreply.github.com>
2021-08-12 19:38:11 +0900

    Update Exercise_04.py

--- python_apis_databases/apis/Exercise_04.py
+++ python_apis_databases/apis/Exercise_04.py
@@ -4,3 +4,22 @@ Write a program that makes a PUT request to update your user information to a ne
 Again make a GET request to confirm that your information has been updated.
 
 '''
+
+import requests 
+
+url = 'http://demo.codingnomads.co:8080/tasks_api/users'
+
+user_info = {
+    'id': 407,
+    'first_name': 'Test',
+    'last_name': 'Info',
+    'email': 'example@example.com'
+}
+
+request_response = requests.put(url, json=user_info)
+
+print(request_response.status_code)
+
+view_new_user = requests.get(url)
+
+print(view_new_user.content)
